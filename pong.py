7b520dfaa2cb00d888b5c8e36bfe4d00f2a1f4a8
Boncroix <boncroix@gmail.com>
2023-08-31 21:33:54 +0200

    feat: toma de contacto con pygame

--- pong.py
+++ pong.py
@@ -7,5 +7,17 @@ salir = False
 while not salir:
     # bucle principal (o main loop)
 
-    # mostrar los camios en la pantalla
+    for evento in pygame.event.get():
+        print('Se ha producido un evento de tipo', evento.type)
+        if evento.type == pygame.QUIT:
+            print('Se ha cerrado la ventana')
+            salir = True
+
+    # renderizar nuestros objetos
+    rectangulo = pygame.Rect(50, 100, 300, 150)
+    pygame.draw.rect(pantalla, (255, 255, 255), rectangulo)
+
+    # mostrar los cambios en la pantalla
     pygame.display.flip()
+
+pygame.quit()
