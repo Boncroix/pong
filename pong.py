b8912d1767e7dfdbe52651b03dd8d670c164d22e
Boncroix <boncroix@gmail.com>
2023-09-03 10:49:13 +0200

    feat: pintar la red, pelota y marcador

--- pong.py
+++ pong.py
@@ -9,12 +9,23 @@ C_FONDO = (100, 100, 100)
 ANCHO_PALA = 20
 ALTO_PALA = 60
 
+ANCHO_RED = 5
+ALTO_RED = 10
+
+CENTRO = (ANCHO/2, ALTO/2)
+RADIO = 10
+
+TAMANO_FUENTE = 100
+POS_MARCADOR1 = (200, 20)
+POS_MARCADOR2 = (520, 20)
+
 MARGEN = 20
 
 
 class Pong:
     def __init__(self):
         pygame.init()
+        pygame.display.set_caption('Ping - Pong')
         self.pantalla = pygame.display.set_mode((800, 600))
 
     def jugar(self):    # contiene el bucle principal
@@ -31,15 +42,38 @@ class Pong:
 
             pygame.draw.rect(self.pantalla, C_FONDO, ((0, 0), (ANCHO, ALTO)))
 
-            # pintar jugador 1 (izqquierda)
+            # pintar jugador 1 (izquierda)
             jugador1 = pygame.Rect(
                 MARGEN, pos_alto_inicial, ANCHO_PALA, ALTO_PALA)
             pygame.draw.rect(self.pantalla, C_OBJETOS, jugador1)
 
-            # pintar jugador 2 (izqquierda)
+            # pintar marcador jugador 1 (izquierda)
+            score1 = '00'
+            marcador1 = pygame.font.Font(None, TAMANO_FUENTE)
+            text1 = marcador1.render(score1, True, C_OBJETOS)
+            self.pantalla.blit(text1, POS_MARCADOR1)
+
+            # pintar jugador 2 (derecha)
             jugador2 = pygame.Rect(
                 ANCHO - MARGEN - ANCHO_PALA, pos_alto_inicial, ANCHO_PALA, ALTO_PALA)
             pygame.draw.rect(self.pantalla, C_OBJETOS, jugador2)
+
+            # pintar marcador jugador 2 (derecha)
+            score2 = '10'
+            marcador2 = pygame.font.Font(None, TAMANO_FUENTE)
+            text2 = marcador2.render(score2, True, C_OBJETOS)
+            self.pantalla.blit(text2, POS_MARCADOR2)
+
+            # pintar la red
+            pos_alto_red = ALTO
+            while pos_alto_red > -5:
+                red = pygame.Rect(ANCHO/2 - ANCHO_RED/2,
+                                  pos_alto_red, ANCHO_RED, ALTO_RED)
+                pygame.draw.rect(self.pantalla, C_OBJETOS, red)
+                pos_alto_red -= 20
+
+            # pintar pelota
+            pygame.draw.circle(self.pantalla, C_OBJETOS, CENTRO, RADIO)
 
             # mostrar los cambios en la pantalla
             pygame.display.flip()
